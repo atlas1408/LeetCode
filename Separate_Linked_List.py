520c9e6ea3f401165dddefdcfdc4436a33ec84a8
Akshay <kshirsagar1408@gmail.com>
2021-02-26 16:44:02 -0500

    Sort linked list

---
+++ Separate_Linked_List.py
@@ -0,0 +1,24 @@
+def sortAbsoluteValues(root):
+	if not root: return root
+	newH = prev = Node(None)
+	newH.next = root
+	newL = None
+	def rec(prev, curr):
+		if not curr:
+			return
+		if curr.val<0:
+			_next = curr
+			prev.next = prev.next.next
+			_next.next = newL
+			newL = _next
+			return rec(prev, prev.next)
+		else:
+			return rec(curr, curr.next)
+	rec(newH, root)
+	temp = newL
+	while temp and temp.next!= None:
+		temp = temp.next
+	if temp:
+		temp.next.next = newH.next
+		return newL
+	return newH
